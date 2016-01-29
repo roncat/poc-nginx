d476d6d64df6331d41fa2a7de562fcfaca6153a4
flavioroncat <flavioroncat@github.com>
2016-01-28 22:09:30 -0200

    Initil config

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
  
-#apt-get update
+apt-get update
 #apt-get remove -y varnish
 #apt-get remove -y nginx
-if ! [ -L /usr/share/nginx/html ]; then
-rm -rf /usr/share/nginx/html
-ln -fs /vagrant /usr/share/nginx/html
-fi
+
+#if ! [ -L /usr/share/nginx/html ]; then
+#	rm -rf /usr/share/nginx/html
+#	ln -fs /vagrant /usr/share/nginx/html
+#fi
 
